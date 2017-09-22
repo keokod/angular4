92ba94b7d42fe84e3521a315de3981ec7c866475
keo <keo@ohkod.fr>
2017-09-22 21:48:01 +0200

    passage un composant vers un autre décorator par @input

--- ng4/src/app/app.module.ts
+++ ng4/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaPremiereComponent } from './ma-premiere/ma-premiere.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HelloWordComponent } from './hello-word/hello-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaPremiereComponent,
+    UserItemComponent,
+    UserListComponent,
+    HelloWordComponent
   ],
   imports: [
     BrowserModule

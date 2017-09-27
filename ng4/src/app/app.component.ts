d03fc15518d0d44c330e92c75fd58e394254ddb8
keo <keo@ohkod.fr>
2017-09-27 08:19:38 +0200

    déplacement fichier dans tete

--- ng4/src/app/app.component.ts
+++ ng4/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public constructor(){
+    console.log(ElementRef.length);
+    this.pousseFooter();
+  }
+
+  pousseFooter(){
+    console.log("---- FOOTER ---");
+
+
+  }
 }

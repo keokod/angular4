c0a078df4263d2e1946e4e6c874a90d2752a0e70
keo <keo@ohkod.fr>
2017-09-28 00:47:42 +0200

    footer pousser vers le bas

--- ng4/src/app/app.module.ts
+++ ng4/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { TeteComponent } from './tete/tete.component';
 import { BurgerComponent } from './tete/burger/burger.component';
 import { BanniereComponent } from './tete/banniere/banniere.component';
 import { LogoComponent } from './tete/logo/logo.component';
-import { MenuComponent } from './tete/menu/menu.component';
-import { PageComponent } from './tete/page/page.component';
-import { FooterComponent } from './tete/footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { PageComponent } from './page/page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
--- ng4/src/app/tete/burger/burger.component.ts
+++ ng4/src/app/tete/burger/burger.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-burger',
   templateUrl: './burger.component.html',
   styleUrls: ['./burger.component.css']
 })
+
+
 export class BurgerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  
+    console.log();
+  }
+
+  burger(event: any) {
+    let saClasse = event.target.classList;
+    console.log(saClasse.length);
+    if(saClasse.length == 1){
+      event.target.classList.add('class3'); // To ADD
+    }else{
+      event.target.classList.remove('change'); // To Remove
+    }
+  }
 
   ngOnInit() {
   }

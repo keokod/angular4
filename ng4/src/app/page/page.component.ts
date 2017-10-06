c45d60d744b682486d034aa975642dee85ef40c6
gpk11 <keo@ohkod.fr>
2017-10-06 11:54:18 +0200

    ajout logo

--- ng4/src/app/page/page.component.ts
+++ ng4/src/app/page/page.component.ts
@@ -10,6 +10,22 @@ export class PageComponent implements OnInit {
 
   @Input() articleCourant;
 
+  public logoEntrer:string[] = [
+    "/assets/logo_angular.png",
+    "/assets/logo_debian.png",
+    "/assets/logo_dollibarr.png",
+    "/assets/logo_langage_web.png",
+    "/assets/logo_lumen.png",
+    "/assets/logo_moodle.png",
+    "/assets/logo_mysql.png",
+    "/assets/logo_node_js.png",
+    "/assets/logo_php.png",
+    "/assets/logo_prestashop.png",
+    "/assets/logo_wordpress.png",
+  ]
+    
+  ;
+
   constructor() {
 
   }
